16198eca9f15bd3ee7d296ed1fef6b87bab1c746
Daec1901 <108996814+Daec1901@users.noreply.github.com>
2025-09-02 14:19:42 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <title>InfiniteDigits</title>
+</head>
+
+<body>
+<h1>Hola mundo</h1>
+  
+</body>
+
+</html>
